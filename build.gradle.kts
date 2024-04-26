a6fdf13470066501c6f3341690b0bb505750cf8e
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-26 08:08:09 +0200

    Add basic structure, files & dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.jvm)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.assertj:assertj-core:3.25.2")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Zombie survivor kata with kotlin
+
+https://github.com/ardalis/kata-catalog/blob/main/katas/Zombie%20Survivors.md
