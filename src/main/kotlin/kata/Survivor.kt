dd771bc4df23132ea6f1422cbb70115e8e5c977c
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-04-27 07:30:24 +0200

    Add scenario for Survivor: Die immediately when receives two wounds

--- src/main/kotlin/kata/Survivor.kt
+++ src/main/kotlin/kata/Survivor.kt
@@ -1,3 +1,16 @@
 package kata
 
-data class Survivor(val name: String, val wounds: Int = 0)
+import kata.Status.ALIVE
+import kata.Status.DEAD
+
+enum class Status {
+    ALIVE, DEAD
+}
+
+data class Survivor(val name: String, val wounds: Int = 0, val status: Status = ALIVE) {
+
+    fun applyWound(): Survivor = when(wounds.inc()) {
+        2 -> this.copy(wounds = wounds.inc(), status = DEAD)
+        else -> this.copy(wounds = wounds.inc())
+    }
+}
--- src/test/kotlin/kata/SurvivorTest.kt
+++ src/test/kotlin/kata/SurvivorTest.kt
@@ -1,6 +1,7 @@
 package kata
 
 import io.kotest.matchers.shouldBe
+import kata.Status.DEAD
 import org.junit.jupiter.api.Test
 
 class SurvivorTest {
@@ -8,5 +9,14 @@ class SurvivorTest {
     @Test
     fun `should create a survivor`() {
         Survivor(name = "Maverick Steel") shouldBe Survivor(name = "Maverick Steel", wounds = 0)
+    }
+
+    @Test
+    fun `should dies immediately when receives two wounds`() {
+        val survivor = Survivor(name = "Maverick Steel")
+
+        val woundedSurvivor = survivor.applyWound().applyWound()
+
+        woundedSurvivor.status shouldBe DEAD
     }
 }
