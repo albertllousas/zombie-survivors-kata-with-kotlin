01d972d474d6b92bd6d3b5d0e864d1ba4204c228
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-05-03 16:32:52 +0200

    Add scenario for Game: be able to add survivors to it at any time

--- src/main/kotlin/kata/Game.kt
+++ src/main/kotlin/kata/Game.kt
@@ -1,6 +1,9 @@
 package kata
 
 data class Game(val survivors: List<Survivor>) {
+
+    fun add(survivor: Survivor) = this.copy(survivors= survivors + survivor)
+
     companion object {
         fun start(): Game = Game(listOf())
     }
--- src/test/kotlin/kata/GameTest.kt
+++ src/test/kotlin/kata/GameTest.kt
@@ -9,4 +9,14 @@ class GameTest {
     fun `should begin with zero survivors`() {
         Game.start() shouldBe Game(survivors = listOf<Survivor>())
     }
+
+    @Test
+    fun `should be able to add survivors to it at any time`() {
+        val game = Game.start()
+        val survivor = Survivor("Maverick Steel")
+
+        val gameWithSurvivors = game.add(survivor)
+
+        gameWithSurvivors shouldBe Game(survivors = listOf(survivor))
+    }
 }
