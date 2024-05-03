0e1b0efc10f5618a174e78966a64520dbfd3f9e7
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-05-03 16:40:30 +0200

    Add scenario for Game: ensure survivor names within a game are unique

--- src/main/kotlin/kata/Game.kt
+++ src/main/kotlin/kata/Game.kt
@@ -1,8 +1,16 @@
 package kata
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+
+data object SurvivorNameAlreadyUsed
+
 data class Game(val survivors: List<Survivor>) {
 
-    fun add(survivor: Survivor) = this.copy(survivors= survivors + survivor)
+    fun add(survivor: Survivor): Either<SurvivorNameAlreadyUsed, Game> =
+        if(survivors.firstOrNull { it.name == survivor.name } != null) SurvivorNameAlreadyUsed.left()
+        else this.copy(survivors= survivors + survivor).right()
 
     companion object {
         fun start(): Game = Game(listOf())
--- src/test/kotlin/kata/GameTest.kt
+++ src/test/kotlin/kata/GameTest.kt
@@ -1,5 +1,8 @@
 package kata
 
+import arrow.core.flatMap
+import arrow.core.left
+import arrow.core.right
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -7,7 +10,7 @@ class GameTest {
 
     @Test
     fun `should begin with zero survivors`() {
-        Game.start() shouldBe Game(survivors = listOf<Survivor>())
+        Game.start() shouldBe Game(survivors = listOf())
     }
 
     @Test
@@ -17,6 +20,16 @@ class GameTest {
 
         val gameWithSurvivors = game.add(survivor)
 
-        gameWithSurvivors shouldBe Game(survivors = listOf(survivor))
+        gameWithSurvivors shouldBe Game(survivors = listOf(survivor)).right()
+    }
+
+    @Test
+    fun `should ensure survivor names within a game are unique`() {
+        val game = Game.start()
+        val survivor = Survivor("Maverick Steel")
+
+        val gameWithSurvivors = game.add(survivor).flatMap { it.add(survivor) }
+
+        gameWithSurvivors shouldBe SurvivorNameAlreadyUsed.left()
     }
 }
