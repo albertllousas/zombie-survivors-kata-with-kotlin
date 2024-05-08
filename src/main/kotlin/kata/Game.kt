8042e900e976b956e7949df83447e80e5bbf56fc
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-05-08 11:48:03 +0200

    Small refactor on Game: Change runTurn method to make it more readable, less functional

--- src/main/kotlin/kata/Game.kt
+++ src/main/kotlin/kata/Game.kt
@@ -8,7 +8,6 @@ import kata.GameStatus.ONGOING
 import kata.Level.BLUE
 import java.time.Clock
 import java.time.LocalDateTime.now
-import java.time.LocalDateTime.parse
 
 enum class GameStatus {
     ONGOING, ENDED
@@ -33,25 +32,26 @@ data class Game(
         else this.copy(survivors = survivors + survivor, history = history + SurvivorAdded(now(clock), survivor.name))
             .let { it.adjustLevel() }.right()
 
-    fun runTurn(survivorName: String, actionOn: (Survivor) -> Either<GameError, Survivor>): Either<GameError, Game> =
-        survivors.find { it.name == survivorName }
-            ?.let { old ->
-                actionOn(old)
-                    .map { it.getAndClearEvents() }
-                    .map { (events, updated) ->
-                        this.copy(
-                            survivors = survivors.map { if (old.name == it.name) updated else old },
-                            history = history + events,
-                        )
-                    }
-            }
-            ?.map { it.adjustStatus() }
-            ?.map { it.adjustLevel() }
-            ?: this.right()
+    fun runTurn(survivorName: String, actionOn: (Survivor) -> Either<GameError, Survivor>): Either<GameError, Game> {
+        val survivor = survivors.find { it.name == survivorName }
+        val result = if (survivor != null) {
+            actionOn(survivor)
+                .map { updatedSurvivor -> updatedSurvivor.getAndClearEvents() }
+                .map { (events, updatedSurvivor) ->
+                    this.copy(
+                        survivors = survivors.map { if (survivor.name == it.name) updatedSurvivor else survivor },
+                        history = history + events,
+                    )
+                }
+                .map { it.adjustStatus() }
+                .map { it.adjustLevel() }
+        } else this.right()
+        return result
+    }
 
     private fun adjustLevel(): Game {
         val newLevel = this.survivors.maxByOrNull { it.level.ordinal }?.level ?: BLUE
-        val newEvents = if(newLevel != level) listOf(GameLeveledUp(now(clock), newLevel)) else emptyList()
+        val newEvents = if (newLevel != level) listOf(GameLeveledUp(now(clock), newLevel)) else emptyList()
         return this.copy(level = newLevel, history = history + newEvents)
     }
 
